691f39bde1314023bc2d995719f368293f6afed5
fran-itria <francoitria01@gmail.com>
2025-02-09 06:34:20 -0300

    Added division

--- mock.ts
+++ mock.ts
@@ -17,5 +17,50 @@ export const divisions = [
             { id: 5, name: 'Team 5' },
             { id: 6, name: 'Team 6' }
         ]
+    },
+    {
+        id: 3,
+        name: 'Masculino C',
+        teams: [
+            { id: 7, name: 'Team 4' },
+            { id: 8, name: 'Team 5' },
+            { id: 9, name: 'Team 6' }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Masculino D',
+        teams: [
+            { id: 10, name: 'Team 4' },
+            { id: 11, name: 'Team 5' },
+            { id: 12, name: 'Team 6' }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Masculino E',
+        teams: [
+            { id: 13, name: 'Team 4' },
+            { id: 14, name: 'Team 5' },
+            { id: 15, name: 'Team 6' }
+        ]
+    },
+    {
+        id: 6,
+        name: 'Femenino A',
+        teams: [
+            { id: 16, name: 'Team 4' },
+            { id: 17, name: 'Team 5' },
+            { id: 18, name: 'Team 6' }
+        ]
+    },
+    {
+        id: 7,
+        name: 'Femenino B',
+        teams: [
+            { id: 19, name: 'Team 4' },
+            { id: 20, name: 'Team 5' },
+            { id: 21, name: 'Team 6' }
+        ]
     }
 ]
