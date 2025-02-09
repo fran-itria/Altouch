ff6c57a2da86029f4efadfb4160dec7f5869c2ce
fran-itria <francoitria01@gmail.com>
2025-02-09 06:34:32 -0300

    Home

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'expo-router';
+import { Link, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 import { Screen } from './Screen';
 
 import { divisions } from '../mock';
@@ -8,22 +8,29 @@ import { divisions } from '../mock';
 export default function App() {
     return (
         <Screen>
-            <Text className='text-white font-bold underline' style={styles.text}>Bienvenidos a la liga Altouch</Text>
-            <Text className='text-white font-bold underline' style={styles.text}>Seleccione el torneo</Text>
-            {divisions.map((division, i) => (
-                <View key={division.id}>
-                    <Link href={`/${division.name}`} asChild>
-                        <Pressable
-                            className='active:bg-red-100 active:border-transparent border-2 rounded-full border-red-300 mt-5 pl-5 pr-5 pb-2 pt-2'>
-                            <Text
-                                className='text-red-300 font-bold'
-                                style={styles.text}
-                            >{division.name}
-                            </Text>
-                        </Pressable>
-                    </Link>
-                </View>
-            ))}
+            <Stack.Screen
+                options={{
+                    headerShown: true
+                }}
+            />
+            <View className='flex flex-col h-screen items-center justify-start p-10'>
+                <Image source={require('../assets/Logo.png')} className='rounded-full w-72 h-72' />
+                <Text className='text-white font-bold underline' style={styles.text}>Seleccione la categoría</Text>
+                {divisions.map((division, i) => (
+                    <View key={division.id}>
+                        <Link href={`/${division.name}`} asChild>
+                            <Pressable
+                                className='active:bg-[#009558] active:border-transparent rounded-full border-red-300 mt-5 pl-5 pr-5 pb-2 pt-2 bg-[#00311D]'>
+                                <Text
+                                    className='text-white font-bold'
+                                    style={styles.text}
+                                >{division.name}
+                                </Text>
+                            </Pressable>
+                        </Link>
+                    </View>
+                ))}
+            </View>
             <StatusBar style="auto" />
         </Screen>
     );
