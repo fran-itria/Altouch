5eb7c6cf221dd834a51c4e4f7ae15a405604c1d1
fran-itria <francoitria01@gmail.com>
2025-03-21 19:12:17 -0300

    Implemented storage firebase and upoload image function

--- app/[liga]/team/[id].tsx
+++ app/[liga]/team/[id].tsx
@@ -1,20 +1,49 @@
 import { Link, Stack, useLocalSearchParams } from "expo-router";
-import { FlatList, Text, View } from "react-native";
+import { Button, FlatList, Image, Platform, Text, View } from "react-native";
 import { Screen } from "../../../components/Screen";
 import { theme } from "../../../tailwind.config";
 import { useEffect, useState } from "react";
-import { getOneTeam, player } from "../../../firebase/services";
+import { getOneTeam, player, updateTeam } from "../../../firebase/services";
+import * as ImagePicker from "expo-image-picker";
+require('../../../assets/Logo.png')
 
 export default function Team() {
-    const { liga, division, team }: { team: string, id: string, liga: string, division: string } = useLocalSearchParams();
+    const { liga, division, team }: { team: string, liga: string, division: string } = useLocalSearchParams();
     const [players, setPlayers] = useState<player[]>([]);
+    const [image, setImage] = useState<string | null>(null);
 
     useEffect(() => {
         (async () => {
             const players = await getOneTeam(liga, division, team)
+            // await updateTeam(team, players[0].team, require('../../../assets/Logo.png'))
             setPlayers(players)
         })()
     }, [])
+
+    const pickImage = async () => {
+        // Solicitar permisos para acceder a la galería
+        if (Platform.OS !== 'web') {
+            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            if (status !== 'granted') {
+                alert('Se requieren permisos para acceder a la galería.');
+                return;
+            }
+        }
+
+        // Seleccionar una imagen
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+
+    useEffect(() => console.log(image), [image])
 
     return (
         <Screen background={theme?.[liga]?.colors?.primary || '#b91c1c'}>
@@ -27,9 +56,6 @@ export default function Team() {
                     headerTitleStyle: { fontWeight: 'bold', color: 'white', fontSize: 25 },
                 }}
             />
-            {/* <Link href={{ pathname: '/player/[id]', params: { id: 1 } }}>
-                <Text> Franco Itria </Text>
-            </Link> */}
             <View className="p-4">
                 <View className={`rounded-t-lg flex flex-row justify-around bg-[#1D7544] p-2 border-b-2 border-b-black`}>
                     <Text className='w-36 text-center font-bold color-white'>Jugadores</Text>
@@ -63,6 +89,13 @@ export default function Team() {
                         </Link>
                     )}
                 />
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <Button title="Seleccionar Imagen" onPress={pickImage} />
+                    {image && <View>
+                        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+                        <Button title="Subir" onPress={() => updateTeam(team, players[0].team, image)}></Button>
+                    </View>}
+                </View>
             </View>
         </Screen>
     )
