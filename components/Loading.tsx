d354dc102683e5c1d1b9a3e5150d20902a72c411
fran-itria <francoitria01@gmail.com>
2025-06-01 11:24:19 -0300

    Animation type in mdodal

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -4,7 +4,7 @@ import { LoadingIcon } from "../Icons";
 
 export default function Loading() {
     return (
-        <Modal transparent={true} visible={true}>
+        <Modal transparent={true} visible={true} animationType="fade">
             <View className='flex justify-center items-center h-screen'>
                 <View
                     className='w-fit h-fit p-5 rounded-full shadow'
