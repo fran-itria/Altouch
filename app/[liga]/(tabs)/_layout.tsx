ed1055231d0976db014c0f99b4e4027f01a84953
fran-itria <francoitria01@gmail.com>
2025-06-03 10:19:41 -0300

    Personal tab bar

--- app/[liga]/(tabs)/_layout.tsx
+++ app/[liga]/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Stack, Tabs, useLocalSearchParams } from "expo-router";
-import { BallIcon, CalendarIcon, FairPlayIcon, PositionsIcon, RegulationIcon } from "../../../Icons";
+import { Stack, Tabs, useLocalSearchParams } from 'expo-router';
 import { theme } from "../../../tailwind.config";
+import MyTabBar from "../../../components/MyTabBar";
 
 export default function TabsLayout() {
     const { liga, division } = useLocalSearchParams() as { liga: string, division: string };
@@ -10,65 +10,40 @@ export default function TabsLayout() {
             <Stack.Screen
                 options={{
                     headerTitle: `${division}`,
-                    headerTitleStyle: { fontWeight: "bold", fontSize: 25, },
+                    headerTitleStyle: { fontWeight: "bold", fontSize: 25, color: theme?.[liga]?.colors?.text },
                     headerTitleAlign: "center",
                 }} />
             <Tabs
+                tabBar={(props) => <MyTabBar {...props} liga={liga} />}
                 screenOptions={{
-                    tabBarActiveTintColor: "",
-                    tabBarInactiveTintColor: "white",
-                    tabBarStyle: { backgroundColor: theme?.[liga]?.colors?.secondary, borderTopWidth: 3, borderColor: theme?.[liga]?.colors?.borderTab },
-                    tabBarIconStyle: { marginTop: 5, marginBottom: 5 },
-                    tabBarLabelStyle: { fontSize: 12, fontWeight: 'bold' },
-                    headerTitleStyle: { fontWeight: 'bold', color: 'white' },
-                    headerStyle: { backgroundColor: theme?.[liga]?.colors?.primary },
                     headerShadowVisible: false,
                     headerTitleAlign: 'center',
-                }}>
+                    headerStyle: { backgroundColor: theme?.[liga]?.colors?.primary },
+                    headerTitleStyle: { fontWeight: 'bold', color: theme?.[liga]?.colors?.text },
+                }}
+            >
                 <Tabs.Screen
                     name="[division]/index"
                     options={{
-                        headerShown: false,
-                        title: "",
-                        tabBarIcon: ({ color }) => <PositionsIcon color={color} />,
-                        animation: "shift",
+                        headerShown: false
                     }}
                 />
                 <Tabs.Screen
                     name="fixture"
-                    options={{
-                        title: "",
-                        tabBarIcon: ({ color }) => <CalendarIcon color={color} />,
-                        animation: "shift",
-                    }}
                     initialParams={{ liga, division }}
                 />
                 <Tabs.Screen
                     name="statics"
-                    options={{
-                        title: "",
-                        tabBarIcon: ({ color }) => <BallIcon color={color} />,
-                        animation: "shift",
-                    }}
                     initialParams={{ liga, division }}
                 />
                 <Tabs.Screen
                     name="fairPlay"
-                    options={{
-                        title: "",
-                        tabBarIcon: ({ color }) => <FairPlayIcon color={color} />,
-                        animation: "shift",
-                    }}
                 />
                 <Tabs.Screen
                     name="regulation"
-                    options={{
-                        title: "",
-                        tabBarIcon: ({ color }) => <RegulationIcon color={color} />,
-                        animation: "shift",
-                    }}
                 />
             </Tabs>
         </>
-    )
-} 
+    );
+}
+
---
+++ components/MyTabBar.tsx
@@ -0,0 +1,93 @@
+import { View } from 'react-native';
+import { useLinkBuilder } from '@react-navigation/native';
+import { PlatformPressable } from '@react-navigation/elements';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { BallIcon, CalendarIcon, FairPlayIcon, PositionsIcon, RegulationIcon } from "../Icons";
+import { theme } from "../tailwind.config";
+import { StyleSheet } from 'nativewind';
+
+export default function MyTabBar({ state, descriptors, navigation, liga }: BottomTabBarProps & { liga: string }) {
+    const { buildHref } = useLinkBuilder();
+
+    const styles = StyleSheet.create({
+        container: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: theme?.[liga]?.colors?.secondary,
+            height: 40
+        },
+        itemFocus: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+        },
+        borderFocusItem: {
+            borderColor: theme?.[liga]?.colors?.tabBarActiveColor,
+            borderWidth: 1,
+            position: 'absolute',
+            top: 0,
+            width: '100%'
+        }
+    })
+
+    function getIcon(name: string, color: string) {
+        switch (name) {
+            case 'fixture':
+                return <CalendarIcon color={color} />;
+            case 'statics':
+                return <BallIcon color={color} />;
+            case 'fairPlay':
+                return <FairPlayIcon color={color} />;
+            case 'regulation':
+                return <RegulationIcon color={color} />;
+            default:
+                return <PositionsIcon color={color} />;
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            {state.routes.map((route, index) => {
+                const { options } = descriptors[route.key];
+
+                const isFocused = state.index === index;
+
+                const onPress = () => {
+                    const event = navigation.emit({
+                        type: 'tabPress',
+                        target: route.key,
+                        canPreventDefault: true,
+                    });
+
+                    if (!isFocused && !event.defaultPrevented) {
+                        navigation.navigate(route.name, route.params);
+                    }
+                };
+
+                const onLongPress = () => {
+                    navigation.emit({
+                        type: 'tabLongPress',
+                        target: route.key,
+                    });
+                };
+
+                return (
+                    <PlatformPressable
+                        href={buildHref(route.name, route.params)}
+                        accessibilityState={isFocused ? { selected: true } : {}}
+                        accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarButtonTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        style={isFocused ? styles.itemFocus : { flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                    >
+                        <View style={isFocused && styles.borderFocusItem}></View>
+                        {getIcon(route.name, isFocused ? theme?.[liga]?.colors.tabBarActiveColor : theme?.[liga]?.colors.tabBarInactiveColor)}
+                    </PlatformPressable>
+                );
+            })}
+        </View>
+    );
+}
